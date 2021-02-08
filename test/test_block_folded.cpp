6574dce16f0951e881882b199ff07efcd70ee164
Joao Paulo Magalhaes <joaoppmagalhaes@gmail.com>
2021-02-08 22:38:23 +0000

    [wip] trying to reproduce #113, no success

--- test/test_block_folded.cpp
+++ test/test_block_folded.cpp
@@ -3,6 +3,60 @@
 namespace c4 {
 namespace yml {
 
+TEST(github113, verification)
+{
+    // https://github.com/biojppm/rapidyaml/issues/113
+    // without terminating newline:
+    auto tree = parse(R"(class: Slicer
+id: slicer
+name: Slicer
+summary: Slices a part into layers.
+description: >
+     A stage that slices a part based on 'flat' surfaces
+     into layers.
+     \<br\>
+     This stage can be used to feed a surface joint
+     that is registered for a perfect fit. It uses
+     the previous stage profile to determine the
+     layers.
+     \<br\>
+     Think of this stage as a step in building your own custom
+     composite material.)");
+
+    const std::string expected = "A stage that slices a part based on 'flat' surfaces"
+     " into layers."
+     " \\<br\\>"
+     " This stage can be used to feed a surface joint"
+     " that is registered for a perfect fit. It uses"
+     " the previous stage profile to determine the"
+     " layers."
+     " \\<br\\>"
+     " Think of this stage as a step in building your own custom"
+     " composite material.";
+
+    EXPECT_EQ(tree["description"].val(), expected);
+
+    // with terminating newline:
+    tree = parse(R"(class: Slicer
+id: slicer
+name: Slicer
+summary: Slices a part into layers.
+description: >
+     A stage that slices a part based on 'flat' surfaces
+     into layers.
+     \<br\>
+     This stage can be used to feed a surface joint
+     that is registered for a perfect fit. It uses
+     the previous stage profile to determine the
+     layers.
+     \<br\>
+     Think of this stage as a step in building your own custom
+     composite material.
+)");
+    EXPECT_EQ(tree["description"].val(), expected + "\n");
+}
+
+
 #define BLOCK_FOLDED_CASES \
     "block folded as seq val, implicit indentation 2", \
     "block folded as map val, implicit indentation 2",\
@@ -16,7 +70,10 @@ namespace yml {
     "block folded as map val, explicit indentation 2, chomp=strip",\
     "block folded as map val, explicit indentation 3",\
     "block folded as map val, explicit indentation 4",\
-    "block folded as map val, explicit indentation 9"
+    "block folded as map val, explicit indentation 9",\
+    "block folded, issue 113, pt1",\
+    "block folded, issue 113, pt2",\
+    "block folded, issue 113, pt3"
 
 
 CASE_GROUP(BLOCK_FOLDED)
@@ -256,7 +313,103 @@ another: val
     N("another", "val")
   }
 ),
-    )
+
+C("block folded, issue 113, pt1",
+R"(class: Slicer
+id: slicer
+name: Slicer
+summary: Slices a part into layers.
+description: >
+     A stage that slices a part based on 'flat' surfaces
+     into layers.
+     This stage can be used to feed a surface joint
+     that is registered for a perfect fit. It uses
+     the previous stage profile to determine the
+     layers.
+     Think of this stage as a step in building your own custom
+     composite material.)",
+  L{
+    N("class", "Slicer"),
+    N("id", "slicer"),
+    N("name", "Slicer"),
+    N("summary", "Slices a part into layers."),
+    N("description", "A stage that slices a part based on 'flat' surfaces"
+     " into layers."
+     " This stage can be used to feed a surface joint"
+     " that is registered for a perfect fit. It uses"
+     " the previous stage profile to determine the"
+     " layers."
+     " Think of this stage as a step in building your own custom"
+     " composite material."),
+  }
+),
+
+C("block folded, issue 113, pt2",
+R"(class: Slicer
+id: slicer
+name: Slicer
+summary: Slices a part into layers.
+description: >
+     A stage that slices a part based on 'flat' surfaces
+     into layers.
+     This stage can be used to feed a surface joint
+     that is registered for a perfect fit. It uses
+     the previous stage profile to determine the
+     layers.
+     Think of this stage as a step in building your own custom
+     composite material.
+)",
+  L{
+    N("class", "Slicer"),
+    N("id", "slicer"),
+    N("name", "Slicer"),
+    N("summary", "Slices a part into layers."),
+    N("description", "A stage that slices a part based on 'flat' surfaces"
+     " into layers."
+     " This stage can be used to feed a surface joint"
+     " that is registered for a perfect fit. It uses"
+     " the previous stage profile to determine the"
+     " layers."
+     " Think of this stage as a step in building your own custom"
+     " composite material.\n"),
+  }
+),
+
+
+C("block folded, issue 113, pt3",
+R"(class: Slicer
+id: slicer
+name: Slicer
+summary: Slices a part into layers.
+description: >
+     A stage that slices a part based on 'flat' surfaces
+     into layers.
+     \<br\>
+     This stage can be used to feed a surface joint
+     that is registered for a perfect fit. It uses
+     the previous stage profile to determine the
+     layers.
+     \<br\>
+     Think of this stage as a step in building your own custom
+     composite material.)",
+  L{
+    N("class", "Slicer"),
+    N("id", "slicer"),
+    N("name", "Slicer"),
+    N("summary", "Slices a part into layers."),
+    N("description", "A stage that slices a part based on 'flat' surfaces"
+     " into layers."
+     " \\<br\\>"
+     " This stage can be used to feed a surface joint"
+     " that is registered for a perfect fit. It uses"
+     " the previous stage profile to determine the"
+     " layers."
+     " \\<br\\>"
+     " Think of this stage as a step in building your own custom"
+     " composite material."),
+  }
+),
+)
 }
 
 INSTANTIATE_GROUP(BLOCK_FOLDED)
